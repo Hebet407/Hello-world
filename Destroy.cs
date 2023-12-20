f3ed500017f8ca8cd58b682dea4ea13bdc07279c
Hebet407 <154345499+Hebet407@users.noreply.github.com>
2023-12-20 16:57:54 +0100

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroys the Object when the game is starting.
+        Destroy(gameObject); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
